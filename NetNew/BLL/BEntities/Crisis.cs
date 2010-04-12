e847eb29739beecae5124ec046f68d50b2f7a2ed
tabareh <tabareh@aa91c83c-13de-11df-ba53-e97ec090ad2f>
2010-04-12 14:44:40 +0000

    finished createcrisis and edit createcrisis

--- NetNew/BLL/BEntities/Crisis.cs
+++ NetNew/BLL/BEntities/Crisis.cs
@@ -3,11 +3,71 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Collections.ObjectModel;
+using System.Reflection;
 
 namespace BLL.BEntities
 {
     public class Crisis : DAL.Crisis
     {
         public ObservableCollection<string> LocationCoordinates = new ObservableCollection<string>();
+
+        public Crisis()
+        {
+           
+            
+        }
+        public Crisis(DAL.Crisis dataObject)
+        {
+            this.Update(dataObject);
+
+        }
+       
+        public BLL.BEntities.Crisis Update(DAL.Crisis dataObject)
+        {
+            
+            Type type = dataObject.GetType();
+            while (type != null)
+            {
+                UpdateForType(type, dataObject, this);
+                type = type.BaseType;
+            }
+            return this;
+        }
+
+
+        private static void UpdateForType(Type type, DAL.Crisis source, BLL.BEntities.Crisis destination)
+        {
+            FieldInfo[] myObjectFields = type.GetFields(
+                BindingFlags.NonPublic | BindingFlags.Public | BindingFlags.Instance);
+
+            foreach (FieldInfo fi in myObjectFields)
+            {
+                fi.SetValue(destination, fi.GetValue(source));
+            }
+        }
+
+        public Utils.Enumerations.CrisisStatuses Status
+        {
+            get
+            {
+                return Utils.Reflection.SafeConvertToEnum<Utils.Enumerations.CrisisStatuses>(StatusVal, Utils.Enumerations.CrisisStatuses.Active);
+            }
+            set
+            {
+                StatusVal = (Int16)value;
+            }
+        }
+        public Utils.Enumerations.LocationTypes LocationType
+        {
+            get
+            {
+                return Utils.Reflection.SafeConvertToEnum<Utils.Enumerations.LocationTypes>(LocationTypeVal, Utils.Enumerations.LocationTypes.Rectangle);
+            }
+            set
+            {
+                LocationTypeVal = (Int16)value;
+            }
+        }
+        
     }
 }
---
+++ NetNew/BLL/BWorkflows/CrisisOperations.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Collections.ObjectModel;
+using Utils.Exceptions;
+
+namespace BLL.BWorkflows
+{
+    public class CrisisOperations
+    {
+        public static BLL.BEntities.Crisis CreateCrisis(string name, string explanation, Utils.Enumerations.CrisisTypes ctype, Utils.Enumerations.LocationTypes locationType
+           , ObservableCollection<string> locationCoordinates)
+        {
+            var c = new DAL.Crisis();
+            c.Name = Utils.Convert.SafeString(name);
+            c.Explanation = Utils.Convert.SafeString(explanation);
+            if (locationCoordinates != null)
+            {
+                c.LocationCoordinatesStr=Utils.Collection.ToString<string>(locationCoordinates);
+            }
+            c.DateCreated = DateTime.Now;
+            c.StatusVal = (short)Utils.Enumerations.CrisisStatuses.Active;
+            c.LocationTypeVal = (short)locationType;
+
+            DAL.Container.Instance.Crises.AddObject(c);
+            DAL.Container.Instance.SaveChanges();
+
+            return new BEntities.Crisis(c);
+        }
+
+
+        public static BLL.BEntities.Crisis UpdateCrisis(int id, string name, string explanation, Utils.Enumerations.CrisisTypes ctype, Utils.Enumerations.LocationTypes locationType, ObservableCollection<string> coords)
+        {
+            var c = DAL.Container.Instance.Crises.FirstOrDefault(cr => cr.Id == id);
+            if (c == null)
+            {
+                throw new VMSException("there is no crisis with such an id");
+            }
+            //Setting new values
+            c.Name = Utils.Convert.SafeString(name);
+            c.Explanation = Utils.Convert.SafeString(explanation);
+            if (coords != null)
+            {
+                c.LocationCoordinatesStr = Utils.Collection.ToString<string>(coords);
+            }
+            c.StatusVal = (short)Utils.Enumerations.CrisisStatuses.Active;
+            c.LocationTypeVal = (short)locationType;
+
+            //Reflecting to DB
+            DAL.Container.Instance.SaveChanges();
+            return new BEntities.Crisis(c);
+        }
+    }
+}
