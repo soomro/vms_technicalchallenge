6fac44a54b0c82c8ab85eed5ab2d13944a5fb462
tabareh <tabareh@aa91c83c-13de-11df-ba53-e97ec090ad2f>
2010-04-23 16:39:24 +0000

    edit profile completed ready to comment and checking inputs

--- trunk/NetNew/UIL/VolReg.aspx.cs
+++ trunk/NetNew/UIL/VolReg.aspx.cs
@@ -14,26 +14,89 @@ public partial class VolReg : PageBase
     {
         if (!IsPostBack)
         {
-            Master.Page.Title = "Volunteer registration";
+            Master.PageTitle = "Volunteer registration";
             ucEnumGender1.EnumType = typeof(Utils.Enumerations.Gender);
             ucEnumGender1.DefaultSelection = Utils.Enumerations.Gender.Man;
+            if (PageAction == PageActions.Edit)
+            {
+                //for test
+                CurrentVolunteer = DAL.Container.Instance.Volunteers.SingleOrDefault(row => row.NameLastName == "text");
+                if (CurrentVolunteer == null)
+                    Response.Redirect("~/" + Constants.PageVolunteerProfile + "?Action=Create");
+                else
+                    FillUiWithVolunteer(CurrentVolunteer);
+                btnRegister.Text = "Update";
+            }
+            else if (PageAction == PageActions.Create)
+            {
+                btnRegister.Text = "Register";
+            }
+            else
+            {
+                Response.Redirect("~/" + Constants.PageVolunteerProfile + "?Action=Create");
+            }
         }
+    }
+
+    private void FillUiWithVolunteer(DAL.Volunteer vol)
+    {
+        txtBirthDate.Text = vol.BirthDate.Value.ToShortDateString();
+        txtCity.Text = vol.Address.City;
+        txtCountry.Text = vol.Address.Country;
+        txtEmailAddress.Text = vol.EmailAddr;
+        txtName.Text = vol.NameLastName;
+        txtOccupation.Text = vol.Occupation;
+        txtUserName.Text=vol.Username;
+        txtUserName.Enabled = false;
+        txtPhone.Text = vol.Phone;
+        txtPostalCode.Text = vol.Address.PostalCode;
+        txtSpecialTraining.Text = vol.EduAndTrainings;
+        txtStreet.Text = vol.Address.Street;
+        txtWeight.Text = vol.Weight.ToString();
+        txtFlatNo.Text = vol.Address.FlatNumber;
+        txtHealthProblem.Text = vol.HealthProb;
+        txtHeight.Text = vol.Height.ToString();
+        txtHouseNo.Text = vol.Address.HouseNumber;
+        ucEnumGender1.DefaultSelection = vol.Gender;
     }
     protected void btnRegister_Click(object sender, EventArgs e)
     {
         //TODO: Check user inputs for format and data
-
-        // make sure there is no such a user with this username
-        var q = DAL.Container.Instance.Volunteers.SingleOrDefault(row => row.Username == txtUserName.Text);
-        if (q != null)
+        if (PageAction==PageActions.Create)
         {
-            Master.ShowMessage(MessageTypes.Error, "There is already such a user in the system. Change the username.");
-            return;
+
+            // make sure there is no such a user with this username
+            var q = DAL.Container.Instance.Volunteers.SingleOrDefault(row => row.Username == txtUserName.Text);
+            if (q != null)
+            {
+                Master.ShowMessage(MessageTypes.Error, "There is already such a user in the system. Change the username.");
+                return;
+            }
+            //make object and fill according to user inputs
+            DAL.Volunteer vol = new DAL.Volunteer();
+            FillVolunteer(vol);
+            //save the object in db
+            DAL.Container.Instance.Volunteers.AddObject(vol);
+            DAL.Container.Instance.SaveChanges();
+            this.CurrentVolunteer = vol;
+            //show message of successfull completion
+            Master.ShowMessage(MessageTypes.Info, "Volunteer registered successfully");
         }
-        //make object and fill according to user inputs
-        DAL.Volunteer vol = new DAL.Volunteer();
+        else if (PageAction == PageActions.Edit)
+        {
+
+        }
+        
+
+    }
+    /// <summary>
+    /// Fills a volunteer object accoridng to user inputs
+    /// </summary>
+    /// <param name="vol">volunteer instance</param>
+    private void FillVolunteer(DAL.Volunteer vol)
+    {
         vol.Address = new DAL.Address();
-        vol.NameLastName = Utils.Convert.SafeString(txtFirstName.Text) + " " + Utils.Convert.SafeString(txtLastName.Text);
+        vol.NameLastName = Utils.Convert.SafeString(txtName.Text);
         vol.BirthDate = Convert.ToDateTime(Utils.Convert.SafeString(txtBirthDate.Text));
         vol.Address.City = Utils.Convert.SafeString(txtCity.Text);
         vol.Address.Country = Utils.Convert.SafeString(txtCountry.Text);
@@ -41,28 +104,20 @@ public partial class VolReg : PageBase
         vol.Address.HouseNumber = Utils.Convert.SafeString(txtHouseNo.Text);
         vol.Address.PostalCode = Utils.Convert.SafeString(txtPostalCode.Text);
         vol.Address.Street = Utils.Convert.SafeString(txtStreet.Text);
-        vol.GenderVal = (short)ucEnumGender1.SelectedValue<Utils.Enumerations.Gender>();
+        vol.Gender = ucEnumGender1.SelectedValue<Utils.Enumerations.Gender>();
         vol.Occupation = Utils.Convert.SafeString(txtOccupation.Text);
         vol.Address.Street = Utils.Convert.SafeString(txtStreet.Text);
         vol.CoordinatesStr = string.Empty;
         vol.CoordinateLastUpdateTime = DateTime.Now;
         vol.LastAccessTime = DateTime.Now;
         vol.EduAndTrainings = txtSpecialTraining.Text;
-        vol.EmailAddr = txtEmalAddress.Text;
+        vol.EmailAddr = txtEmailAddress.Text;
         vol.Username = txtUserName.Text;
         vol.Password = txtPassword.Text;
         vol.Weight = Convert.ToDecimal(txtWeight.Text);
         vol.Height = Convert.ToDecimal(txtHeight.Text);
         vol.HealthProb = txtHealthProblem.Text;
         vol.Phone = txtPhone.Text;
-        
-
-        //save the object in db
-        DAL.Container.Instance.Volunteers.AddObject(vol);
-        DAL.Container.Instance.SaveChanges();
-
-        //redirecting to volunteer's profile page
-        Response.Redirect("~/volProfile.aspx");
     }
     protected void btnCancel_Click(object sender, EventArgs e)
     {
