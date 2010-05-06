6980de2da73b2198c5de707362914c6f01874118
tabareh <tabareh@aa91c83c-13de-11df-ba53-e97ec090ad2f>
2010-05-06 19:56:40 +0000

    created

---
+++ NetNew/UIL/UControls/UCSearchVolunteer.ascx.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class UControls_UCSearchVolunteer : System.Web.UI.UserControl
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (!IsPostBack)
+        {
+            lstVolunteers.DataSource = DAL.Container.Instance.Volunteers;
+            lstVolunteers.DataTextField = "NameLastName";
+            lstVolunteers.DataValueField = "Id";
+            lstVolunteers.DataBind();
+        }
+    }
+    public List<int> SelectedVolunteers
+    {
+        get
+        {
+            var list = new List<int>();
+            foreach (ListItem item in lstVolunteers.Items)
+            {
+                if (item.Selected == true)
+                    list.Add(Convert.ToInt32(item.Value));
+            }
+            return list;
+        }
+    }
+    public string SelectedVolunteersString
+    {
+        get
+        {
+            return Utils.Collection.ToString<int>(SelectedVolunteers);
+        }
+    }
+}
